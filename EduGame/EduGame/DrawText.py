df1549d811d54abdb1d19ce27c598637a5eea6d1
Amar Kisoensingh <unrulyeon314@gmail.com>
2017-12-07 19:33:01 +0100

    Finished player movement and assets
    Finished the code for the movement of the player and made assets. The player and assets needs to be converted to sprites so they can interact with each other

---
+++ EduGame/EduGame/DrawText.py
@@ -0,0 +1,33 @@
+import os, sys
+import logging
+import pygame as pg
+from pygame.locals import *
+from MainGame import *
+from LoadAssets import *
+from MainMenu import *
+
+class DrawText(object):
+
+    def __init__(self):
+        logging.info("INIT DRAW")
+        self.label = ""
+        self.size = 12
+        self.color = (255, 255, 255)
+        self.posx = 0
+        self.posy = 0
+        self.screen = pg.display.get_surface()
+        self.grid_width = 50
+        self.grid_height = 50
+
+    def rt(self, tscreen, tlabel, tsize, tfont, tcolor, tposx, tposy, aa):
+        self.font = pg.font.Font(tfont, tsize)
+        self.label = str(tlabel)
+        self.label = self.font.render(self.label, aa, tcolor)
+        tscreen.blit(self.label, (tposx, tposy))
+
+    def rtcenter(self, tscreen, tlabel, tsize, tfont, tcolor, tposx, tposy, aa):
+        self.font = pg.font.Font(tfont, tsize)
+        self.label = str(tlabel)
+        self.label = self.font.render(self.label, aa, tcolor)
+        self.label_center = self.label.get_rect(center=(screen_width / 2 + tposx, screen_height / 2 + tposy))
+        tscreen.blit(self.label, self.label_center)
