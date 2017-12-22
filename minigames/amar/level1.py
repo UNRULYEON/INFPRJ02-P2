63743fd83f54c19d348a18a7b103577632c9f31c
Amar Kisoensingh <unrulyeon314@gmail.com>
2017-12-22 01:12:53 +0100

    Completed level 1

--- minigames/amar/level1.py
+++ minigames/amar/level1.py
@@ -4,23 +4,49 @@ from random import randint
 
 def gen_problem(self):
     print("Generating level 1 problem...")
-    a, b, c = random.sample(range(1, 11), 3)
 
-    sort_answer = randint(1, 5)
+    global sort_answer
+    global sort_answer_1
+    global sort_answer_2
+    global answer
+    global extra
+
+    answer = 0
+    sort_answer = 0
+    sort_answer_1 = ""
+    sort_answer_2 = ""
+
+    a, b, c, extra= random.sample(range(1, 11), 4)
+
+    sort_answer = randint(1, 4)
     if sort_answer is 1:
         sort_answer = "a + b + c"
-        answer = a + b + c
+        sort_answer_1 = "+"
+        sort_answer_2 = "+"
+        x = a + b
+        y = x + c
+        answer = y
     elif sort_answer is 2:
         sort_answer = "a - b + c"
-        answer = a - b + c
+        sort_answer_1 = "-"
+        sort_answer_2 = "+"
+        x = a - b
+        y = x + c
+        answer = y
     elif sort_answer is 3:
         sort_answer = "a + b - c"
-        answer = a + b - c
+        sort_answer_1 = "+"
+        sort_answer_2 = "-"
+        x = a + b
+        y = x = c
+        answer = y
     elif sort_answer is 4:
         sort_answer = "a - b - c"
-        answer = a - b - c
-    elif sort_answer is 5:
-        sort_answer = "a + b + c"
-        answer = a + b + c
+        sort_answer_1 = "-"
+        sort_answer_2 = "-"
+        x = a - b
+        y = x - c
+        answer = y
 
-    return a, b, c, sort_answer, answer
+    print("Returning level 1 problem...")
+    return a, b, c, sort_answer, sort_answer_1, sort_answer_2, answer, extra
--- minigames/amar/minigameAmar.py
+++ minigames/amar/minigameAmar.py
@@ -4,7 +4,9 @@ import logging
 import pygame as pg
 from pygame.locals import *
 import MainGame as mg
+import LoadAssets as la
 from minigames.amar import level1
+import random
 from random import randint, choice
 
 # LOGGING SETTINGS
@@ -28,6 +30,10 @@ screen_caption = "School Roamer - Amar"
 # Delete this for prod
 screen_size = screen_width, screen_height = 800, 600
 
+# ASSETS
+bg, bg_rect = la.asset("bg.png")
+black = (0, 0, 0)
+problem_height = 167.625
 
 def asset(name):
     fullname = os.path.join("assets", name)
@@ -73,7 +79,11 @@ class Problem(object):
         self.font = pg.font.Font(None, 30)
         self.fsize = 16
         print("Player is level: " + str(self.level))
-        print("Player has completed " + str(self.minigame_amar) + " minigames")
+        print("Completed?:  " + str(self.minigame_amar))
+        global checkButton
+        checkButton = pg.rect.Rect(0, 0, 300, 50)
+        checkButton.centerx = 400
+        checkButton.centery = 75
         self.reinit()
 
     def reinit(self):
@@ -81,15 +91,35 @@ class Problem(object):
         if self.level is 1:
             print("Creating level 1 problem")
 
+            global a
+            global b
+            global c
             global answer
             global numbers
             global posNum
             global choiceNumbers
             global posNumGuess1
             global posNumGuess2
+            global sort_answer
+            global sort_answer_1
+            global sort_answer_2
+
+            global rectangle_1
+            global rectangle_1_rect
+            global rectangle_1_dragging
+            global rectangle_1_number
+            global rectangle_2
+            global rectangle_2_rect
+            global rectangle_2_dragging
+            global rectangle_2_number
+            global rectangle_3
+            global rectangle_3_rect
+            global rectangle_3_dragging
+            global labelRandom
+
             numbers = []
             choiceNumbers = []
-            a, b, c, sort_answer, answer = level1.gen_problem(self)
+            a, b, c, sort_answer, sort_answer_1, sort_answer_2, answer, extra = level1.gen_problem(self)
 
             numbers.append(a)
             choiceNumbers.append(a)
@@ -108,9 +138,29 @@ class Problem(object):
 
             posNum = numbers.index(choiceNumbers[0])
 
+            rectangle_1, rectangle_1_rect = la.asset("sn.png")
+            rectangle_1_rect.x = 137.75
+            rectangle_1_rect.y = 300
+            rectangle_1_dragging = False
+            rectangle_1_number = numbers[posNumGuess1]
+
+            rectangle_2, rectangle_2_rect = la.asset("sn.png")
+            rectangle_2_rect.x = 350
+            rectangle_2_rect.y = 300
+            rectangle_2_dragging = False
+            rectangle_2_number = numbers[posNumGuess2]
+
+            rectangle_3, rectangle_3_rect = la.asset("sn.png")
+            rectangle_3_rect.x = 562.25
+            rectangle_3_rect.y = 300
+            rectangle_3_dragging = False
+            labelRandom = str(extra)
+
+            answer = str(answer)
+
             logging.info(str(a) + ", " + str(b) + ", " + str(c) + ", " + str(sort_answer) + ", " + str(answer))
             print(str(a) + ", " + str(b) + ", " + str(c) + ", " + str(sort_answer) + ", " + str(answer))
-            print("Number the user has to put in the right place: " + str(guess1) + " and " + str(guess2))
+            print("Numbers the user has to put in the right place: " + str(guess1) + " and " + str(guess2))
 
         elif self.level is 2:
             print("Creating level 2 problem")
@@ -121,7 +171,133 @@ class Problem(object):
         elif self.level is 5:
             print("Creating level 5 problem")
 
+    def check(self):
+        print("------------------")
+        print("Checking answer...")
+        if self.level is 1:
+            print("Level 1 problem")
+
+            global posNum
+            global sort_answer
+
+            a = 0
+            b = 0
+            c = 0
+
+            print("Rectangle 1: " + str(rectangle_1_rect))
+            print("Rectangle 1 pos1: " + str(rectangle_1_rect.collidepoint(187.5, problem_height)))
+            print("Rectangle 1 pos2: " + str(rectangle_1_rect.collidepoint(375.5, problem_height)))
+            print("Rectangle 1 pos3: " + str(rectangle_1_rect.collidepoint(563.5, problem_height)))
+            print("Rectangle 2: " + str(rectangle_2_rect))
+            print("Rectangle 2 pos1: " + str(rectangle_2_rect.collidepoint(187.5, problem_height)))
+            print("Rectangle 2 pos2: " + str(rectangle_2_rect.collidepoint(375.5, problem_height)))
+            print("Rectangle 2 pos3: " + str(rectangle_2_rect.collidepoint(563.5, problem_height)))
+
+            if rectangle_1_rect.collidepoint(187.5, problem_height):
+                print("Rectangle 1 is in pos 1")
+                a = rectangle_1_number
+                print("a is: " + str(a))
+                if rectangle_2_rect.collidepoint(375.5, problem_height) and posNum is 2:
+                    print("Rectangle 2 is in pos 2")
+                    b = rectangle_2_number
+                    print("b is: " + str(b))
+                    c = numbers[posNum]
+                    print("c is: " + str(c))
+                elif rectangle_2_rect.collidepoint(563.5, problem_height) and posNum is 1:
+                    print("Rectangle 2 is in pos 3")
+                    c = rectangle_2_number
+                    print("c is: " + str(c))
+                    b = numbers[posNum]
+                    print("b is: " + str(b))
+            elif rectangle_1_rect.collidepoint(375.5, problem_height):
+                print("Rectangle 1 is in pos 2")
+                b = rectangle_1_number
+                print("b is: " + str(b))
+                if rectangle_2_rect.collidepoint(187.5, problem_height) and posNum is 2:
+                    print("Rectangle 2 is in pos 1")
+                    a = rectangle_2_number
+                    print("a is: " + str(a))
+                    c = numbers[posNum]
+                    print("c is: " + str(c))
+                elif rectangle_2_rect.collidepoint(563.5, problem_height) and posNum is 0:
+                    print("Rectangle 2 is in pos 3")
+                    c = rectangle_2_number
+                    print("c is: " + str(c))
+                    a = numbers[posNum]
+                    print("a is: " + str(a))
+            elif rectangle_1_rect.collidepoint(563.5, problem_height):
+                print("Rectangle 1 is in pos 3")
+                c = rectangle_1_number
+                print("c is: " + str(c))
+                if rectangle_2_rect.collidepoint(187.5, problem_height) and posNum is 1:
+                    print("Rectangle 2 is in pos 1")
+                    a = rectangle_2_number
+                    print("a is: " + str(a))
+                    b = numbers[posNum]
+                    print("b is: " + str(b))
+                    print("Rectangle 2 wasn't set!")
+                elif rectangle_2_rect.collidepoint(375.5, problem_height) and posNum is 0:
+                    print("Rectangle 2 is in pos 2")
+                    b = rectangle_2_number
+                    print("b is: " + str(b))
+                    a = numbers[posNum]
+                    print("a is: " + str(a))
+            else:
+                print("rect1 was not set")
+
+            if sort_answer == "a + b + c":
+                print("Sort answer is " + str(sort_answer))
+                a = a + b
+                a = a + c
+                a = str(a)
+                print(str(a))
+                if a == answer:
+                    print("Correct!")
+                else:
+                    print("Wrong!")
+            elif sort_answer == "a - b + c":
+                print("Sort answer is " + str(sort_answer))
+                a = a - b
+                a = a + c
+                a = str(a)
+                print(str(a))
+                if a == answer:
+                    print("Correct!")
+                else:
+                    print("Wrong!")
+            elif sort_answer == "a + b - c":
+                print("Sort answer is " + str(sort_answer))
+                a = a + b
+                a = a = c
+                a = str(a)
+                print(str(a))
+                if a == answer:
+                    print("Correct!")
+                else:
+                    print("Wrong!")
+            elif sort_answer == "a - b - c":
+                print("Sort answer is " + str(sort_answer))
+                a = a - b
+                a = a - c
+                a = str(a)
+                print(str(a))
+                if a == answer:
+                    print("Correct!")
+                else:
+                    print("Wrong!")
+
     def control(self):
+
+        global checkButton
+        global rectangle_1_dragging
+        rectangle_1_dragging = rectangle_1_dragging
+        global rectangle_2_dragging
+        rectangle_2_dragging = rectangle_2_dragging
+        global rectangle_3_dragging
+        rectangle_3_dragging = rectangle_3_dragging
+        offset_x = -50
+        offset_y = -50
+
         for event in pg.event.get():
 
             # QUIT
@@ -130,14 +306,48 @@ class Problem(object):
                 Minigame.state = False
 
             # MOUSE UP
-            if event.type == pg.MOUSEBUTTONUP:
+            elif event.type == pg.MOUSEBUTTONUP:
                 logging.info("mouse up")
-                print("mouse up")
+                if event.button == 1:
+                    rectangle_1_dragging = False
+                    rectangle_2_dragging = False
+                    rectangle_3_dragging = False
 
             # MOUSE DOWN
-            if event.type == pg.MOUSEBUTTONDOWN:
+            elif event.type == pg.MOUSEBUTTONDOWN:
                 logging.info("mouse down")
-                print("mouse down")
+                if event.button == 1:
+                    if rectangle_1_rect.collidepoint(event.pos):
+                        rectangle_1_dragging = True
+                        mouse_x, mouse_y = event.pos
+                        offset_x = rectangle_1_rect.x - mouse_x
+                        offset_y = rectangle_1_rect.y - mouse_y
+                    if rectangle_2_rect.collidepoint(event.pos):
+                        rectangle_2_dragging = True
+                        mouse_x, mouse_y = event.pos
+                        offset_x = rectangle_2_rect.x - mouse_x
+                        offset_y = rectangle_2_rect.y - mouse_y
+                    if rectangle_3_rect.collidepoint(event.pos):
+                        rectangle_3_dragging = True
+                        mouse_x, mouse_y = event.pos
+                        offset_x = rectangle_3_rect.x - mouse_x
+                        offset_y = rectangle_3_rect.y - mouse_y
+                    if checkButton.collidepoint(event.pos):
+                        self.check()
+
+            elif event.type == pg.MOUSEMOTION:
+                if rectangle_1_dragging:
+                    mouse_x, mouse_y = event.pos
+                    rectangle_1_rect.x = mouse_x + offset_x
+                    rectangle_1_rect.y = mouse_y + offset_y
+                if rectangle_2_dragging:
+                    mouse_x, mouse_y = event.pos
+                    rectangle_2_rect.x = mouse_x + offset_x
+                    rectangle_2_rect.y = mouse_y + offset_y
+                if rectangle_3_dragging:
+                    mouse_x, mouse_y = event.pos
+                    rectangle_3_rect.x = mouse_x + offset_x
+                    rectangle_3_rect.y = mouse_y + offset_y
 
             # KEYDOWN
             elif event.type == KEYDOWN:
@@ -148,33 +358,67 @@ class Problem(object):
                     Minigame.state = False
 
     def render(self):
-        self.draw.rt(self.screen, "Level: " + str(self.level), 30, None, mg.white, 715, 10, 1)
-        if posNum is 0:
-            self.draw.rt(self.screen, numbers[posNum], 60, None, mg.white, 300, 300, 1)
-            if posNumGuess1 is 1:
-                self.draw.rt(self.screen, numbers[posNumGuess1], 60, None, mg.white, 400, 300, 1)
-                self.draw.rt(self.screen, numbers[posNumGuess2], 60, None, mg.white, 500, 300, 1)
-            if posNumGuess1 is 2:
-                self.draw.rt(self.screen, numbers[posNumGuess1], 60, None, mg.white, 500, 300, 1)
-                self.draw.rt(self.screen, numbers[posNumGuess2], 60, None, mg.white, 400, 300, 1)
-        elif posNum is 1:
-            self.draw.rt(self.screen, numbers[posNum], 60, None, mg.white, 400, 300, 1)
-            if posNumGuess1 is 0:
-                self.draw.rt(self.screen, numbers[posNumGuess1], 60, None, mg.white, 300, 300, 1)
-                self.draw.rt(self.screen, numbers[posNumGuess2], 60, None, mg.white, 500, 300, 1)
-            if posNumGuess1 is 2:
-                self.draw.rt(self.screen, numbers[posNumGuess1], 60, None, mg.white, 500, 300, 1)
-                self.draw.rt(self.screen, numbers[posNumGuess2], 60, None, mg.white, 300, 300, 1)
-        if posNum is 2:
-            self.draw.rt(self.screen, numbers[posNum], 60, None, mg.white, 500, 300, 1)
-            if posNumGuess1 is 0:
-                self.draw.rt(self.screen, numbers[posNumGuess1], 60, None, mg.white, 300, 300, 1)
-                self.draw.rt(self.screen, numbers[posNumGuess2], 60, None, mg.white, 400, 300, 1)
-            if posNumGuess1 is 1:
-                self.draw.rt(self.screen, numbers[posNumGuess1], 60, None, mg.white, 400, 300, 1)
-                self.draw.rt(self.screen, numbers[posNumGuess2], 60, None, mg.white, 300, 300, 1)
+        self.draw.rt(self.screen, "Level: " + str(self.level), 30, None, black, 695, 30, 1)
 
-        self.draw.rt(self.screen, "=       " + str(answer), 60, None, mg.white, 600, 300, 1)
+        if posNum is 0:
+            self.draw.rt(self.screen, numbers[posNum], 70, None, black, 187.5, problem_height, 1)
+            if posNumGuess1 is 1:
+                self.draw.rt(self.screen, numbers[posNumGuess1], 70, None, black, 375.5, problem_height, 1)
+                self.draw.rt(self.screen, numbers[posNumGuess2], 70, None, black, 563.25, problem_height, 1)
+            if posNumGuess1 is 2:
+                self.draw.rt(self.screen, numbers[posNumGuess1], 70, None, black, 563.25, problem_height, 1)
+                self.draw.rt(self.screen, numbers[posNumGuess2], 70, None, black, 375.5, problem_height, 1)
+        elif posNum is 1:
+            self.draw.rt(self.screen, numbers[posNum], 70, None, black, 375.5, problem_height, 1)
+            if posNumGuess1 is 0:
+                self.draw.rt(self.screen, numbers[posNumGuess1], 70, None, black, 187.5, problem_height, 1)
+                self.draw.rt(self.screen, numbers[posNumGuess2], 70, None, black, 563.25, problem_height, 1)
+            if posNumGuess1 is 2:
+                self.draw.rt(self.screen, numbers[posNumGuess1], 70, None, black, 563.25, problem_height, 1)
+                self.draw.rt(self.screen, numbers[posNumGuess2], 70, None, black, 187.5, problem_height, 1)
+        elif posNum is 2:
+            self.draw.rt(self.screen, numbers[posNum], 70, None, black, 563.25, problem_height, 1)
+            if posNumGuess1 is 0:
+                self.draw.rt(self.screen, numbers[posNumGuess1], 70, None, black, 187.5, problem_height, 1)
+                self.draw.rt(self.screen, numbers[posNumGuess2], 70, None, black, 375.5, problem_height, 1)
+            if posNumGuess1 is 1:
+                self.draw.rt(self.screen, numbers[posNumGuess1], 70, None, black, 375.5, problem_height, 1)
+                self.draw.rt(self.screen, numbers[posNumGuess2], 70, None, black, 187.5, problem_height, 1)
+
+        self.draw.rt(self.screen, sort_answer_1, 70, None, black, 281.25, problem_height, 1)
+        self.draw.rt(self.screen, str(sort_answer_2), 70, None, black, 469.375, problem_height, 1)
+        self.draw.rt(self.screen, "= " + str(answer), 70, None, black, 650, problem_height, 1)
+
+        # Sticky Note 1
+        self.screen.blit(rectangle_1, rectangle_1_rect)
+
+        self.font = pg.font.Font(None, 70)
+        label1 = str(numbers[posNumGuess1])
+        label1 = self.font.render(label1, 1, (0, 0, 0))
+        self.screen.blit(label1, ((rectangle_1_rect.centerx - (label1.get_rect().width / 2)), (rectangle_1_rect.centery - (label1.get_rect().height / 2))))
+
+        # Sticky Note 2
+        self.screen.blit(rectangle_2, rectangle_2_rect)
+
+        self.font = pg.font.Font(None, 70)
+        label2 = str(numbers[posNumGuess2])
+        label2 = self.font.render(label2, 1, (0, 0, 0))
+        self.screen.blit(label2, ((rectangle_2_rect.centerx - (label2.get_rect().width / 2)), (rectangle_2_rect.centery - (label2.get_rect().height / 2))))
+
+        # Sticky Note 3
+        self.screen.blit(rectangle_3, rectangle_3_rect)
+
+        self.font = pg.font.Font(None, 70)
+        self.labelRandom = self.font.render(labelRandom, 1, (0, 0, 0))
+        self.screen.blit(self.labelRandom, ((rectangle_3_rect.centerx - (self.labelRandom.get_rect().width / 2)), (rectangle_3_rect.centery - (self.labelRandom.get_rect().height / 2))))
+
+        # Check button
+        pg.draw.rect(self.screen, (0, 0, 0), checkButton)
+
+        self.font = pg.font.Font(None, 50)
+        labelcheck = self.font.render("Check answer", 1, (255, 255, 255))
+        self.screen.blit(labelcheck, ((checkButton.centerx - (labelcheck.get_rect().width / 2)), (checkButton.centery - (labelcheck.get_rect().height / 2))))
+
         pg.display.flip()
 
 
@@ -192,7 +436,8 @@ class Minigame(object):
         self.problem = Problem()
 
     def render(self):
-        self.screen.fill((12, 75, 22))
+        #self.screen.fill((12, 75, 22))
+        self.screen.blit(bg, (0,0))
         self.problem.render()
         pg.display.flip()
 
